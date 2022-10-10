1ed87a4d572db85d8084c9b5d000bc97dae0d9d2
Robbe Michiels <robbe.michiels@capgemini.com>
2022-10-10 11:58:18 +0200

    Enable Swagger in prod

--- Lovensedotnet/LovenseApiUI/Startup.cs
+++ Lovensedotnet/LovenseApiUI/Startup.cs
@@ -49,9 +49,10 @@ namespace LovenseApiUI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Driver v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Driver v1"));
 
             app.UseHttpsRedirection();
 
